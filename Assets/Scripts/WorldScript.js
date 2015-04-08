043b6599b6f88082525472303ce412f6cdbe852d
Eric Miller <eric@legoaces.org>
2015-04-08 16:14:01 -0400

    Added global level grid

--- Assets/Scripts/WorldScript.js
+++ Assets/Scripts/WorldScript.js
@@ -1,9 +1,36 @@
 ﻿#pragma strict
 
+static var worldGrid:Wrapper[,];
+
+enum SquareType {EMPTY, OFF_GRID, ENEMY_BASE, HOME_BASE, TOWER, WALL};
+
+class Wrapper
+{
+	var type:SquareType;
+	var gameObject:GameObject;
+	
+	function Wrapper(type:SquareType, gameObject){
+		this.type = type;
+		this.gameObject = gameObject;
+	}
+	
+	function Wrapper(){
+		this.type = SquareType.EMPTY;
+		this.gameObject = new GameObject();
+	}
+}
+
+
+
 function Start () {
- //Look here! A comment!
+	worldGrid = new Wrapper[20,12];
+	for (var x = 0; x < worldGrid.GetLength(0); x++){
+		for (var y = 0; y < worldGrid.GetLength(1); y++){
+			worldGrid[x,y] = new Wrapper();
+		}
+	}
 }
 
-function Update () {
-
-}
+//function Update () {
+//
+//}
