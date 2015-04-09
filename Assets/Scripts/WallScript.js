226c2e12e284bf42f3dbffe003f415879e5a0839
Eric Miller <eric@legoaces.org>
2015-04-09 09:17:13 -0400

    Allowed walls to create their own references in the game world.

---
+++ Assets/Scripts/WallScript.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var health:float = 100;
+@HideInInspector
+var inited:boolean = false;
+
+function Start () {
+	inited = false;
+}
+
+function Update () {
+	if (!inited){
+		WorldScript.addObject(SquareType.WALL, gameObject);
+		inited = true;
+	}
+	if (health<=0){
+		WorldScript.worldGrid[transform.position.x, transform.position.z] = new Wrapper();
+		GameObject.Destroy(gameObject, 0);
+	}
+}
