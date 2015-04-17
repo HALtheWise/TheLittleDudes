75f62ecdbb85d51161d67295ee38f152aba226cf
Eric Miller <eric@legoaces.org>
2015-04-17 01:35:35 -0400

    Got tower attacking working with moderately fancy animation.

--- Assets/Scripts/TowerAttackScript.js
+++ Assets/Scripts/TowerAttackScript.js
@@ -1,9 +1,32 @@
 ﻿#pragma strict
 
 function Start () {
-
+	tryAttacks();
 }
 
 function Update () {
 
+}
+
+var DAMAGE:float;
+var ATTACK_RADIUS:float;
+
+var attackParticles:GameObject;
+
+function tryAttacks() {
+	while(true){
+		var ants = GameObject.FindGameObjectsWithTag("Ant");
+		
+		var didAttack = false;
+		for (var i = 0; i < ants.Length; i++){
+			if (Vector3.Distance(ants[i].transform.position, transform.position) <= ATTACK_RADIUS){
+				ants[i].GetComponent(AntScript).health -= DAMAGE;
+				didAttack = true;
+			}
+		}
+		if (didAttack){
+			GameObject.Instantiate(attackParticles, transform.position, transform.rotation);
+		}
+		yield WaitForSeconds(0.4);
+	}
 }
