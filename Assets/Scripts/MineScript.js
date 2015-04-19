6f8ddfb19b3b298adf5250ee11a02b5149d12b2e
Eric Miller <eric@legoaces.org>
2015-04-19 01:10:37 -0400

    Added and moved a bunch of prefabs

---
+++ Assets/Scripts/MineScript.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+function Start () {}
+
+@HideInInspector 
+var oldState = WorldScript.gameState;
+function Update () {
+	if (WorldScript.gameState == GameMode.BUILD_PHASE && oldState != GameMode.BUILD_PHASE){
+		produceResources();
+	}
+	oldState = WorldScript.gameState;
+}
+
+var strength:int = 5;
+function produceResources(){
+	Debug.Log("producing");
+	Debug.Log(WorldScript.getSquare(transform.position).type);
+	var tile = WorldScript.getSquare(transform.position);
+	if (tile.resource == Resource.NONE){ Debug.Log("Mine not on resource!"); return;}
+	WorldScript.resources[tile.resource + 0] += strength;
+	Debug.Log("Now has " + WorldScript.resources[tile.resource + 0].ToString() + " " + 
+			Resource.GetName(Resource, tile.resource));
+}
---
+++ Assets/Scripts/ResourceManager.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Start () {}
+
+function Update () {}
+
+static function deductResources(air:int, earth:int, fire:int, water:int){
+	WorldScript.resources[Resource.AIR+0] -= air;
+	WorldScript.resources[Resource.EARTH+0] -= earth;
+	WorldScript.resources[Resource.FIRE+0] -= fire;
+	WorldScript.resources[Resource.WATER+0] -= water;
+}
